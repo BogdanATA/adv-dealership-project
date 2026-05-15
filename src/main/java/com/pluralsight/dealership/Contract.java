95ce0adadc313ab9fb0c965b90e1aac8be817040
BogdanATA <batameev@gmail.com>
2026-05-15 13:53:45 -0400

    created the `Contract` abstract parent class with `getTotalPrice` and `getMonthlyPayment` abstract methods

---
+++ src/main/java/com/pluralsight/dealership/Contract.java
@@ -0,0 +1,53 @@
+package com.pluralsight.dealership;
+
+import java.time.LocalDate;
+
+public abstract class Contract {
+    private LocalDate date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+
+    public Contract(LocalDate date, String customerName, String customerEmail, Vehicle vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public abstract double getTotalPrice();
+
+    public abstract double getMonthlyPayment();
+}
